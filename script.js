0ac3ffc8ab2cf907dada3dec26cf008542812de2
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-06-22 01:01:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ function volume_sphere() {
 	var volume;
 	var radius = document.getElementById("radius").value;
 	volume = (4/3) * Math.PI * Math.pow(radius, 3);
+	volume = volume.toFixed(4);
 	document.getElementById("volume").value = volume;
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
